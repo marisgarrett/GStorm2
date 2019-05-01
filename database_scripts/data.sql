769de38108ad68726003e05a0dcc2499b4b3bf76
Maris Garrett <maris.garrett@hotmail.com>
2019-05-01 09:30:23 +0100

    Added Demo Data

--- database_scripts/data.sql
+++ database_scripts/data.sql
@@ -28,8 +28,75 @@ INSERT INTO `product_category` (`product_category_id`, `name`) VALUES ('12', 'Ni
 /*
     Products Data
 */
-INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('1', 'PS4', 'A playstation 4 console', '229.85', '4', '1');
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('1', 'PS4', 'A playstation 4 console', '229.85', '4', '0');
 
 INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('2', 'PS4 Pro', 'A PS4 pro', '339.85', '4', '1');
 
 INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('3', 'God of War', 'God of war game', '49.95', '5', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('13', 'Madden 20', 'Madden 20', '54.99', '5', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('14', 'Ghost Giant', 'Ghost Giant', '24.99', '5', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('15', 'PlayStation Dual Shock 4 Controller', 'PlayStation Dual Shock 4 Controller', '49.99', '6', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('16', 'PlayStation Move Controller Twin Pack', 'PlayStation Move Controller Twin Pack', '128.44', '6', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('17', 'Astro A20 Wireless Gaming Headset', 'Astro A20 Wireless Gaming Headset', '149.99', '6', '0');
+
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('4', 'Xbox One X with Tom Clancys The Division 2', 'Buy the Xbox One X Tom Clancy’s The Division 2 Bundle and put the fate of the free world in your hands. Lead your team of elite agents to save a country on the brink of collapse as you explore the open, dynamic, and hostile world of post-pandemic Washington, DC. This bundle includes a full-game download of Tom Clancy’s The Division 2, a 1-month Xbox Live Gold subscription, and a 1-month Xbox Game Pass trial. With true 4K gaming and 40% more power than any other console, there’s never been a better time to game with Xbox One X.', '449.99', '1', '1');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('5', 'Xbox One X', 'With true 4K gaming and 40% more power than any other console, there’s never been a better time to game with Xbox One X.', '385', '1', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('6', 'Xbox One S', 'Xbox One S', '188.95', '1', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('7', 'Tom Clancys The Division 2', 'Tom Clancys The Division 2', '57.99', '2', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('8', 'Borderlands 3 - Deluxe Edition', 'Borderlands 3 - Deluxe Edition', '89.99', '2', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('9', 'F1 2019', 'F1 2019', '54.99', '2', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('10', 'Xbox One Controller', 'Xbox One Controller', '59.99', '3', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('11', 'Xbox One Elite Controller', 'Xbox One Elite Controller', '139.99', '3', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('12', 'Turtle Beach Headset', 'Turtle Beach Headset', '219.99', '3', '0');
+
+
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('18', 'Nintendo Switch Red-Blue', 'Nintendo Switch Red-Blue', '279.99', '10', '1');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('19', 'Nintendo Switch Grey', 'Nintendo Switch Grey', '279.99', '10', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('20', 'Yoshis Crafted World', 'Yoshis Crafted World', '44.99', '11', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('21', 'Hollow Knight', 'Hollow Knight', '29.99', '11', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('22', 'FIFA 19', 'FIFA 19', '49.99', '11', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('23', 'Joy-Con Pair Neon Green-Pink', 'Joy-Con Pair Neon Green-Pink', '69.99', '12', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('24', 'Nintendo Switch Wireless Pro Controller', 'Nintendo Switch Wireless Pro Controller', '64.99', '12', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('25', 'Hyper X Cloud 2 Pro', 'Hyper X Cloud 2 Pro', '66.99', '12', '0');
+
+
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('26', 'Fierce EXILE gaming PC', 'Fierce EXILE gaming PC', '588.95', '7', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('27', 'Yoyotech Spartan A8', 'Yoyotech Spartan A8', '464.99', '7', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('28', 'Acer Nitro 5', 'Acer Nitro 5', '814.99', '7', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('29', 'Age of Wonders: Planetfall', 'Age of Wonders: Planetfall', '41.99', '8', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('30', 'The Elder Scrolls Online: Elsweyr', 'The Elder Scrolls Online: Elsweyr', '49.99', '8', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('31', 'Total War: Three Kingdoms', 'Total War: Three Kingdoms', '45.99', '8', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('32', 'HyperX Alloy Gaming Keyboard', 'HyperX Alloy Gaming Keyboard', '99.99', '9', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('33', 'Trust GXT 130 Wireless Gaming Mouse', 'Trust GXT 130 Wireless Gaming Mouse', '17.99', '9', '0');
+
+INSERT INTO `product` (`product_id`, `name`, `description`, `price`, `category_id`, `is_featured`) VALUES ('34', 'Arctis Pro Wireless Headset', 'Arctis Pro Wireless Headset', '299.99', '9', '0');
